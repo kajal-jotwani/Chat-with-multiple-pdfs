0d340f4ca2e9a82b564a8d5843d81918c7f586ee
kajal-jotwani <kajaljotwani06@gmail.com>
2025-05-22 17:10:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+
+def main():
+    print("🎉 Your Python environment is set up correctly!")
+    print(f"Current working directory: {os.getcwd()}")
+
+if __name__ == "__main__":
+    main()
